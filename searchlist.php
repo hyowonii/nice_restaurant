9659f1e1d3ec31ff896731c54e4415f267deec0e
S01-21 <lop080@ewhain.net>
2021-11-13 16:47:16 +0900

    third commit

--- searchlist.php
+++ searchlist.php
@@ -1,3 +1,10 @@
+<?php
+    $mysqli=mysqli_connect("localhost", "team08", "team08", "team08");
+    $sql_search = "insert into searchkey (searchKey) values('".$_GET["search"]."')";
+    $res = mysqli_query($mysqli, $sql_search);
+    $res_row = mysqli_fetch_array($res);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +16,29 @@
     <link rel="stylesheet" href="searchlist.css">
 </head>
 <body>
-    <div>
+<header>
+    <h1><a href="/index.html">웹사이트 이름</a></h1>
+    <nav><h4>모범음식점 검색 결과입니다.</h4></nav>
+</header>
+<section>
+    <div class="list_result">
+    <div class="result_box">
+        <a href="/restaurant_detail.html" class="info_name">식당이름</a>
+        <span class="info_category">업태</span>
+        <p class="info_address">주소</p>
     </div>
+    </div>
+</section>
+<div id="line"></div>
+<aside>
+    <form id="menu">
+        <div class="mymenu">
+            <input type="button" value="마이페이지" onclick="location.href='mypage.html'">
+            <input type="button" value="리뷰" onclick="location.href='reviewAll.html'">
+            <input type="button" value="착한가격식당" onclick="location.href='kind_price.html'">
+            <input type="button" value="순위" onclick="location.href='ranking.html'">
+        </div>
+    </form>
+    </aside>
 </body>
 </html>
